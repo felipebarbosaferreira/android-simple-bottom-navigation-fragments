c35cc2b37318bacb8706a236a7892e8c48abbf1e
C1281192 Felipe Barbosa Ferreira <c1281192@interno.bb.com.br>
2020-02-14 17:56:14 -0300

    transicoes entre os fragmentos sendo realizadas

--- app/src/main/java/br/com/movietalks/simplebottomnavigationfragments/MainActivity.kt
+++ app/src/main/java/br/com/movietalks/simplebottomnavigationfragments/MainActivity.kt
@@ -14,13 +14,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val toolbar: Toolbar? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        setSupportActionBar(toolbar)
 
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
@@ -34,32 +30,33 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener =
         BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
-            menuItem.onNavDestinationSelected(findNavController(R.id.mainHostNavigationFragment))
-                    || super.onOptionsItemSelected(menuItem)
+// desse modo o fragmento de start aparece em cada transicao de menu da bottom
+//            menuItem.onNavDestinationSelected(findNavController(R.id.mainHostNavigationFragment))
+//                    || super.onOptionsItemSelected(menuItem)
 
-//            when (menuItem.itemId) {
-//                R.id.blogFragment -> {
-//                    val fragment = BlogFragment()
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.mainHostNavigationFragment, fragment, fragment.javaClass.getSimpleName())
-//                        .commit()
-//                    return@OnNavigationItemSelectedListener true
-//                }
-//                R.id.chapterFragment -> {
-//                    val fragment = ChapterFragment()
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.mainHostNavigationFragment, fragment, fragment.javaClass.getSimpleName())
-//                        .commit()
-//                    return@OnNavigationItemSelectedListener true
-//                }
-//                R.id.storeFragment -> {
-//                    val fragment = StoreFragment()
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.mainHostNavigationFragment, fragment, fragment.javaClass.getSimpleName())
-//                        .commit()
-//                    return@OnNavigationItemSelectedListener true
-//                }
-//            }
-//            false
+            when (menuItem.itemId) {
+                R.id.blogFragment -> {
+                    val fragment = BlogFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainHostNavigationFragment, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.chapterFragment -> {
+                    val fragment = ChapterFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainHostNavigationFragment, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.storeFragment -> {
+                    val fragment = StoreFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainHostNavigationFragment, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
         }
 }
